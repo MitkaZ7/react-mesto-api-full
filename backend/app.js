1dac7c720b9126bdd78f1a8e043735249c6f923e
MitkaS7 <mitka.dev@gmail.com>
2021-11-14 23:39:55 +0300

    dev: try again

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors, celebrate, Joi } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/NotFoundError');
@@ -15,63 +15,20 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'https://locus.nomoredomains.rocks',
-  'http://locus.nomoredomains.rocks',
-  'https://178.154.193.242',
-  'http://178.154.193.242',
-];
+const options = {
+  origin: [
+    'https://locus.nomoredomains.rocks',
+    'http://locus.nomoredomains.rocks',
+    'http://localhost:3000',
+  ],
+  methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Origin', 'Authorization'],
+  credentials: true,
+};
 
-app.use((req, res, next) => {
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE';
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  next();
-});
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin(origin, callback) {
-//       if (allowedCors.includes(origin) || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//   }),
-// );
-
-// const options = {
-//   origin: [
-//     'https://locus.nomoredomains.rocks',
-//     'http://locus.nomoredomains.rocks',
-//     'http://localhost:3000',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
-
-// app.options('*', cors(options));
+app.options('*', cors(options));
 
 app.use(cookieParser());
 
