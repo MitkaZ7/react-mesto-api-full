2cd51da85874151ff4ae1ed85f1fd57437a9053d
MitkaS7 <mitka.dev@gmail.com>
2021-11-14 09:53:03 +0300

    dev: try again

--- backend/app.js
+++ backend/app.js
@@ -2,30 +2,42 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors, celebrate, Joi } = require('celebrate');
-const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
-const options = {
-  origin: [
-    'https://praktikum.tk',
-    'http://praktikum.tk',
-    'https://locus.nomoredomains.rocks',
-    'http://locus.nomoredomains.rocks',
-    'http://localhost:3000',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'https://locus.nomoredomains.rocks',
+  'http://locus.nomoredomains.rocks',
+  'http://localhost:3000',
+];
 
 const app = express();
-app.use('*', cors(options));
+app.use((req, res, next) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+});
 
 app.use(cookieParser());
 app.use(requestLogger);
